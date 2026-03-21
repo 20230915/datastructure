eacdb3c47794dda7a25ee36a5c235ca2bce0cb2a
20230915 <147494528+20230915@users.noreply.github.com>
2026-03-21 15:11:24 +0900

    Add files via upload

---
+++ 20230915_array_report1.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+double calAver(int scores[], int size);
+int getStud(int scores[], int size, int studentNumber);
+void printScore(int scores[], int size, double, int option);
+
+int main(void) {
+    int scores[30];
+    int studentNumber;
+    double average;
+
+    srand(time(NULL));
+    for(int i=0; i<30; i++) {
+        scores[i] = rand() % 101;
+    }
+
+    average = calAver(scores, 30);
+    printf("학급 평균 점수: %.2f\n", average);
+    printf("학생 번호를 입력하세요 (1~30): ");
+    scanf("%d", &studentNumber);
+
+    int score = getStud(scores, 30, studentNumber);
+    if (score != -1) {
+        printf("%d번 학생의 점수: %d\n", studentNumber, score);
+    } else {
+        printf("유효하지 않은 학생 번호입니다.\n");
+    }
+
+    printScore(scores, 30, average, 0);
+    printScore(scores, 30, average, 1);
+    printScore(scores, 30, average, 23);
+
+    return 0;
+}
+
+double calAver(int scores[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += scores[i];
+    }
+    return sum / (double)size;
+}
+
+int getStud(int scores[], int size, int studentNumber) {
+    if (studentNumber >= 1 && studentNumber <= size) {
+        return scores[studentNumber - 1];
+    } else {
+        return -1;
+    }
+}
+
+void printScore(int score[], int size, double average, int option) {
+    switch(option) {
+        case 0:
+            for(int i = 0; i < size; i++) {
+                printf("학 생 번 호: %d, 점 수: %d\n", i+1, score[i]);
+            }
+            break;
+        case 1:
+            printf("평균이상 학생\n");
+            for(int i = 0; i < size; i++) {
+                if(score[i] >= average) {
+                    printf("학 생 번 호: %d, 점 수: %d\n", i+1, score[i]);
+                }
+            }
+            break;
+        case 2:
+            printf("평균미만 학생\n");
+            for(int i = 0; i < size; i++) {
+                if(score[i] < average) {
+                    printf("학 생 번 호: %d, 점 수: %d\n", i+1, score[i]);
+                }
+            }
+            break;
+        default:
+            printf("유효하지 않은 옵션입니다.\n");
+            break;
+    }
+}
