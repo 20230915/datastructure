00ea7c776d97e85e92e933113c1ae25a55d30a90
20230915 <147494528+20230915@users.noreply.github.com>
2026-04-03 10:39:57 +0900

    Add files via upload

---
+++ stack.c
@@ -0,0 +1,62 @@
+#include "stack.h"
+#include <stdio.h>
+
+void initStack(Stack *s)
+{
+    s->top = -1;
+}
+
+int isEmpty(Stack *s)
+{
+    return s->top == -1;
+}
+
+int isFull(Stack *s)
+{
+    return s->top == MAX - 1;
+}
+
+void push(Stack *s, int value)
+{
+    if (isFull(s))
+    {
+        printf("Stack oveflow! Cannot push %d\n", value);
+        return;
+    }
+    s->data[++(s->top)] = value;
+}
+
+int pop(Stack *s)
+{
+    if (isEmpty(s))
+    {
+        printf("Stack underflow! Cannot pop\n");
+        return -1;
+    }
+    return s->data[(s->top)--];
+}
+
+int peek(Stack *s)
+{
+    if (isEmpty(s))
+    {
+        printf("Stack is empty! Cannot peek\n");
+        return -1;
+    }
+    return s->data[s->top];
+}
+
+void display(Stack *s)
+{
+    if (isEmpty(s))
+    {
+        printf("Stack is empty\n");
+        return;
+    }
+    printf("Stack contents: ");
+    for (int i = 0; i <= s->top; i++)
+    {
+        printf("%d", s->data[i]);
+    }
+    printf("\n");
+}
